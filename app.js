e173df430ff459f49967a40ad34c14b9ece87bc9
Mateus <josequeiroz@estudante.ufscar.br>
2024-02-07 15:58:20 -0300

    refatoração e simplificação de trechos do código

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./app_server/routes/API');
+var indexRouter = require('./app_server/routes/Routes');
+
 
 
 var app = express();
--- app_server/routes/Routes.js
+++ app_server/routes/Routes.js
@@ -1,25 +1,10 @@
 
 var express = require('express');
-var router = express.Router();
-var ctrlAPI = require("../controllers/API");
 const StatsControl = require('../controllers/StatsController');
 var statsControl = new StatsControl;
-
-//GET home page. 
-//router.get('/', function(req, res, next) {
-//  res.render('index', { title: 'Express' });
-//});
-
+var router = express.Router();
 
 //Métodos para a ultilização da API 
-
-//GET
-router.get('/API/CS', ctrlAPI.getChallengeStats);
-//PUT
-router.put('/API/CS',ctrlAPI.updateChallengeStats);
-
-
-//stats
 router.post('/stats/saveChallengeStats',statsControl.saveChallengeStats);
 router.post('/stats/saveTimeStats',statsControl.saveTimeStats);
 
